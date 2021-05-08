109553e0382b4566fa48c7671c553d2053490f1b
Boryza727 <81522113+Boryza727@users.noreply.github.com>
2021-05-08 13:52:32 +0200

    Update Github_file.py

--- Github_file.py
+++ Github_file.py
@@ -10,7 +10,7 @@ remainder5 = int(input())
 remainder7 = int(input())
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 print("Your age is ", age, "; that's a good time to start programming!")
-print("Now I will prove to you that I can count to any number you want.")
+print("Now I will prove to you that I can count to any number you want. Insert a number: ")
 number = int(input())
 n = 0
 while n <= number:
