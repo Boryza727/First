4b081bcac126c24aa479a2867a90a4b5d1aab2bc
Boryza727 <81522113+Boryza727@users.noreply.github.com>
2021-05-09 19:14:50 +0200

    Update Github_file.py

--- Github_file.py
+++ Github_file.py
@@ -49,3 +49,39 @@ if light in traffic_lights:
         print("Just wait.")
 else:
     print("No such traffic light color, do whatever you want")
+    
+print("How many animals you can afford? ")
+print("indicate the amount to spend")
+chicken = 23
+goat = 678
+pig = 1296
+cow = 3848
+sheep = 6769
+Sum = int(input())
+if (Sum / sheep) >= 1:
+    if (Sum / sheep) >= 2:
+        print(int(Sum / sheep), str("sheep"))
+    else:
+        print(int(Sum / sheep), str("sheep"))
+elif (Sum / cow) >= 1:
+    if (Sum / cow) >= 2:
+        print(int(Sum / cow), str("cows"))
+    else:
+        print(int(Sum / cow), str("cow"))
+elif (Sum / pig) >= 1:
+    if (Sum / pig) >= 2:
+        print(int(Sum / pig), str("pigs"))
+    else:
+        print(int(Sum / pig), str("pig"))
+elif (Sum / goat) >= 1:
+    if (Sum / goat) >= 2:
+        print(int(Sum / goat), str("goats"))
+    else:
+        print(int(Sum / goat), str("goat"))
+elif Sum / chicken >= 1:
+    if (Sum / chicken) >= 2:
+        print(int(Sum / chicken), str("chickens"))
+    else:
+        print(int(Sum / chicken), str("chicken"))
+elif (Sum / chicken) < 1:
+    print("None")
